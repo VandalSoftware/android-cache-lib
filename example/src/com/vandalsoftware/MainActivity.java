7c0646d4b7a3863981154fe494785eec72331f08
Jonathan Le <jonathanle@gmail.com>
2013-02-03 17:10:33 -0800

    Adds an example selection list for the main activity.

--- example/src/com/vandalsoftware/MainActivity.java
+++ example/src/com/vandalsoftware/MainActivity.java
@@ -16,217 +16,78 @@
 
 package com.vandalsoftware;
 
-import android.app.Activity;
-import android.os.AsyncTask;
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.text.Editable;
-import android.text.TextUtils;
-import android.text.TextWatcher;
-import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-import com.vandalsoftware.io.DiskLruCache;
-import com.vandalsoftware.io.IoUtils;
-import com.vandalsoftware.io.Streams;
-
-import java.io.File;
-import java.io.IOException;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 /**
  * @author Jonathan Le
  */
-public class MainActivity extends Activity {
-    private static final String TAG = "Example";
-    private DiskLruCache mDiskCache;
-    private EditText mKeyEdit;
-    private EditText mValueEdit;
-    private Button mSetValueBtn;
-    private Handler mHandler = new Handler();
-    private Runnable mFetchRunnable = new FetchRunnable();
-
-    /**
-     * Called when the activity is first created.
-     */
+public class MainActivity extends ListActivity {
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        mKeyEdit = (EditText) findViewById(R.id.key);
-        mKeyEdit.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View view, boolean hasFocus) {
-                if (!hasFocus && TextUtils.isEmpty(mValueEdit.getText())) {
-                    fetchValue(mKeyEdit.getText().toString());
-                }
-            }
-        });
-        mKeyEdit.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                mValueEdit.getText().clear();
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                mHandler.removeCallbacks(mFetchRunnable);
-                mHandler.postDelayed(mFetchRunnable, 500);
-            }
-        });
-        mValueEdit = (EditText) findViewById(R.id.value);
-        mSetValueBtn = (Button) findViewById(R.id.btn_set_value);
-        mSetValueBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                new CacheSaveTask().execute(new KeyValue(mKeyEdit.getText().toString(),
-                        mValueEdit.getText().toString()));
-            }
-        });
-        new CacheLoadTask().execute();
+        final ListItem[] items = {
+                new ListItem(new Intent(this, SetValueActivity.class),
+                        getString(R.string.set_value_label))
+        };
+        setListAdapter(new ListItemAdapter(items));
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        IoUtils.closeQuietly(mDiskCache);
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        final ListItem item = (ListItem) l.getItemAtPosition(position);
+        startActivityForResult(item.intent, 0);
     }
 
-    private void fetchValue(String s) {
-        new CacheReadTask().execute(s);
-    }
+    private static class ListItem {
+        public final Intent intent;
+        public final String label;
 
-    private class FetchRunnable implements Runnable {
-        @Override
-        public void run() {
-            fetchValue(mKeyEdit.getText().toString());
-        }
-    }
-
-    private static class KeyValue {
-        public final String key;
-        public final String value;
-        public boolean hasErrors;
-
-        private KeyValue(String key, String value) {
-            this.key = key;
-            this.value = value;
+        private ListItem(Intent intent, String label) {
+            this.intent = intent;
+            this.label = label;
         }
     }
 
-    private class CacheSaveTask extends AsyncTask<KeyValue, Void, KeyValue> {
+    private static class ListItemAdapter extends BaseAdapter {
+        private final ListItem[] mItems;
+
+        public ListItemAdapter(ListItem[] items) {
+            mItems = items;
+        }
+
         @Override
-        protected void onPostExecute(KeyValue kv) {
-            if (kv.hasErrors) {
-                Toast.makeText(MainActivity.this, getString(R.string.save_error_msg_fmt, kv.key),
-                        Toast.LENGTH_SHORT).show();
-            } else {
-                if (TextUtils.isEmpty(kv.value)) {
-                    Toast.makeText(MainActivity.this, getString(R.string.removed_msg_fmt, kv.key),
-                            Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(MainActivity.this, getString(R.string.saved_msg_fmt, kv.key),
-                            Toast.LENGTH_SHORT).show();
-                }
-                mKeyEdit.setText(null);
-                mKeyEdit.requestFocus();
-                mValueEdit.setText(null);
+        public int getCount() {
+            return mItems.length;
+        }
+
+        @Override
+        public ListItem getItem(int pos) {
+            return mItems[pos];
+        }
+
+        @Override
+        public long getItemId(int pos) {
+            return pos;
+        }
+
+        @Override
+        public View getView(int pos, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.list_row_view, parent, false);
             }
-        }
-
-        @Override
-        protected KeyValue doInBackground(KeyValue... kvs) {
-            final KeyValue kv = kvs[0];
-            try {
-                if (TextUtils.isEmpty(kv.value)) {
-                    mDiskCache.remove(kv.key);
-                } else {
-                    DiskLruCache.Editor e = mDiskCache.edit(kv.key);
-                    Streams.writeStringTo(e.newOutputStream(0), kv.value);
-                    e.commit();
-                }
-                return kv;
-            } catch (IOException e) {
-                kv.hasErrors = true;
-                return kv;
-            } finally {
-                try {
-                    mDiskCache.flush();
-                } catch (IOException ignored) {
-                }
-            }
-        }
-    }
-
-    private class CacheReadTask extends  AsyncTask<String, Void, String> {
-        @Override
-        protected void onPreExecute() {
-            mHandler.removeCallbacks(mFetchRunnable);
-        }
-
-        @Override
-        protected void onPostExecute(String s) {
-            if (s != null && TextUtils.isEmpty(mValueEdit.getText())) {
-                mValueEdit.setText(s);
-                mValueEdit.setSelection(s.length());
-            }
-        }
-
-        @Override
-        protected String doInBackground(String... strings) {
-            String key = strings[0];
-            DiskLruCache.Snapshot s = null;
-            try {
-                s = mDiskCache.get(key);
-                if (s != null) {
-                    return Streams.readStringFrom(s.getInputStream(0));
-                } else {
-                    return null;
-                }
-            } catch (IOException e) {
-                return null;
-            } finally {
-                IoUtils.closeQuietly(s);
-            }
-        }
-    }
-
-    private class CacheLoadTask extends AsyncTask<Void, Void, DiskLruCache> {
-        @Override
-        protected void onPreExecute() {
-            setEnabled(false);
-        }
-
-        @Override
-        protected void onPostExecute(DiskLruCache diskLruCache) {
-            if (diskLruCache != null) {
-                setEnabled(true);
-            } else {
-                Toast.makeText(MainActivity.this, R.string.error, Toast.LENGTH_LONG).show();
-            }
-            mDiskCache = diskLruCache;
-        }
-
-        @Override
-        protected DiskLruCache doInBackground(Void... voids) {
-            File dir = new File(getCacheDir(), "example");
-            try {
-                return DiskLruCache.open(dir, 1, 1, 5 * 1024);
-            } catch (IOException e) {
-                Log.w(TAG, "open", e);
-                return null;
-            }
-        }
-
-        private void setEnabled(boolean enabled) {
-            mKeyEdit.setEnabled(enabled);
-            mValueEdit.setEnabled(enabled);
-            mSetValueBtn.setEnabled(enabled);
+            final TextView v = (TextView) convertView;
+            v.setText(mItems[pos].label);
+            return convertView;
         }
     }
 }
---
+++ example/src/com/vandalsoftware/SetValueActivity.java
@@ -0,0 +1,232 @@
+/*
+ * Copyright (C) 2012 Vandal Software
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.vandalsoftware;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.vandalsoftware.io.DiskLruCache;
+import com.vandalsoftware.io.IoUtils;
+import com.vandalsoftware.io.Streams;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author Jonathan Le
+ */
+public class SetValueActivity extends Activity {
+    private static final String TAG = "Example";
+    private DiskLruCache mDiskCache;
+    private EditText mKeyEdit;
+    private EditText mValueEdit;
+    private Button mSetValueBtn;
+    private Handler mHandler = new Handler();
+    private Runnable mFetchRunnable = new FetchRunnable();
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.set_value);
+        mKeyEdit = (EditText) findViewById(R.id.key);
+        mKeyEdit.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (!hasFocus && TextUtils.isEmpty(mValueEdit.getText())) {
+                    fetchValue(mKeyEdit.getText().toString());
+                }
+            }
+        });
+        mKeyEdit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                mValueEdit.getText().clear();
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                mHandler.removeCallbacks(mFetchRunnable);
+                mHandler.postDelayed(mFetchRunnable, 500);
+            }
+        });
+        mValueEdit = (EditText) findViewById(R.id.value);
+        mSetValueBtn = (Button) findViewById(R.id.btn_set_value);
+        mSetValueBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new CacheSaveTask().execute(new KeyValue(mKeyEdit.getText().toString(),
+                        mValueEdit.getText().toString()));
+            }
+        });
+        new CacheLoadTask().execute();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        IoUtils.closeQuietly(mDiskCache);
+    }
+
+    private void fetchValue(String s) {
+        new CacheReadTask().execute(s);
+    }
+
+    private class FetchRunnable implements Runnable {
+        @Override
+        public void run() {
+            fetchValue(mKeyEdit.getText().toString());
+        }
+    }
+
+    private static class KeyValue {
+        public final String key;
+        public final String value;
+        public boolean hasErrors;
+
+        private KeyValue(String key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private class CacheSaveTask extends AsyncTask<KeyValue, Void, KeyValue> {
+        @Override
+        protected void onPostExecute(KeyValue kv) {
+            if (kv.hasErrors) {
+                Toast.makeText(SetValueActivity.this, getString(R.string.save_error_msg_fmt, kv.key),
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                if (TextUtils.isEmpty(kv.value)) {
+                    Toast.makeText(SetValueActivity.this, getString(R.string.removed_msg_fmt, kv.key),
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(SetValueActivity.this, getString(R.string.saved_msg_fmt, kv.key),
+                            Toast.LENGTH_SHORT).show();
+                }
+                mKeyEdit.setText(null);
+                mKeyEdit.requestFocus();
+                mValueEdit.setText(null);
+            }
+        }
+
+        @Override
+        protected KeyValue doInBackground(KeyValue... kvs) {
+            final KeyValue kv = kvs[0];
+            try {
+                if (TextUtils.isEmpty(kv.value)) {
+                    mDiskCache.remove(kv.key);
+                } else {
+                    DiskLruCache.Editor e = mDiskCache.edit(kv.key);
+                    Streams.writeStringTo(e.newOutputStream(0), kv.value);
+                    e.commit();
+                }
+                return kv;
+            } catch (IOException e) {
+                kv.hasErrors = true;
+                return kv;
+            } finally {
+                try {
+                    mDiskCache.flush();
+                } catch (IOException ignored) {
+                }
+            }
+        }
+    }
+
+    private class CacheReadTask extends  AsyncTask<String, Void, String> {
+        @Override
+        protected void onPreExecute() {
+            mHandler.removeCallbacks(mFetchRunnable);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            if (s != null && TextUtils.isEmpty(mValueEdit.getText())) {
+                mValueEdit.setText(s);
+                mValueEdit.setSelection(s.length());
+            }
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String key = strings[0];
+            DiskLruCache.Snapshot s = null;
+            try {
+                s = mDiskCache.get(key);
+                if (s != null) {
+                    return Streams.readStringFrom(s.getInputStream(0));
+                } else {
+                    return null;
+                }
+            } catch (IOException e) {
+                return null;
+            } finally {
+                IoUtils.closeQuietly(s);
+            }
+        }
+    }
+
+    private class CacheLoadTask extends AsyncTask<Void, Void, DiskLruCache> {
+        @Override
+        protected void onPreExecute() {
+            setEnabled(false);
+        }
+
+        @Override
+        protected void onPostExecute(DiskLruCache diskLruCache) {
+            if (diskLruCache != null) {
+                setEnabled(true);
+            } else {
+                Toast.makeText(SetValueActivity.this, R.string.error, Toast.LENGTH_LONG).show();
+            }
+            mDiskCache = diskLruCache;
+        }
+
+        @Override
+        protected DiskLruCache doInBackground(Void... voids) {
+            File dir = new File(getCacheDir(), "example");
+            try {
+                return DiskLruCache.open(dir, 1, 1, 5 * 1024);
+            } catch (IOException e) {
+                Log.w(TAG, "open", e);
+                return null;
+            }
+        }
+
+        private void setEnabled(boolean enabled) {
+            mKeyEdit.setEnabled(enabled);
+            mValueEdit.setEnabled(enabled);
+            mSetValueBtn.setEnabled(enabled);
+        }
+    }
+}
